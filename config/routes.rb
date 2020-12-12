5072fc6623029a5c22e55e2983ababa09c6420db
weiklr <weik.liang@gmail.com>
2020-12-13 00:54:29 +0800

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  # This new pattern below routes a GET request for the URL /help 
+  # to the help action in the Static Pages controller. 
+  # As with the rule for the root route, this creates two named routes, 
+  # help_path and help_url:
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
