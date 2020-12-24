62d9c4c3168a7a4962bfba78a2f3672ff9285911
weiklr <weik.liang@gmail.com>
2020-12-24 15:59:03 +0800

    finish 12.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   # This new pattern below routes a GET request for the URL /help 
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
   # generates all GET, POST, PUT, PATCH, DELETE urls
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
