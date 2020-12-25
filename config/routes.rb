0f697242f210cde4aba18d570729e0c61230e1de
weiklr <weik.liang@gmail.com>
2020-12-25 15:27:25 +0800

    added comments on generating models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,21 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
-  # This new pattern below routes a GET request for the URL /help 
-  # to the help action in the Static Pages controller. 
-  # As with the rule for the root route, this creates two named routes, 
+  # This new pattern below routes a GET request for the URL /help
+  # to the help action in the Static Pages controller.
+  # As with the rule for the root route, this creates two named routes,
   # help_path and help_url:
   get     '/about',   to: 'static_pages#about'
   get     '/help',    to: 'static_pages#help'
   get     '/contact', to: 'static_pages#contact'
   get     '/signup',  to: 'users#new'
-  
+
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
   # generates all GET, POST, PUT, PATCH, DELETE urls
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :password_resets,     only: %i[new create edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # This code is not idiomatically correct.
-    @micropost = @user.microposts.build(content: "Lorem ipsum")
+    @micropost = @user.microposts.build(content: 'Lorem ipsum')
   end
 
   test 'should be valid' do
