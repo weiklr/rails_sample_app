29c732ea3a04543c15ab36c0273d1bab130cf309
weiklr <weik.liang@gmail.com>
2020-12-27 01:52:32 +0800

    finish 14.1.5

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com', 
+    @user = User.new(name: 'Example User', email: 'user@example.com',
                      password: 'foobar', password_confirmation: 'foobar')
   end
 
@@ -14,7 +14,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'name should be present' do
-    @user.name = '     '    
+    @user.name = '     '
     assert_not @user.valid?
   end
 
@@ -24,67 +24,81 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'name should not be too long' do
-    @user.name = "a" * 51
+    @user.name = 'a' * 51
     assert_not @user.valid?
   end
 
   test 'email should not be too long' do
-    @user.email = "a" * 244 + '@example.com'
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end
 
   test 'email validation should accept valid addresses' do
-    valid_addresses = %w[user@example.com USER@foo.COM  A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 
-    test 'email validation should reject invalid addresses' do
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                             foo@bar_baz.com foo@bar+baz.com]
-      invalid_addresses.each do |invalid_address|
-        @user.email = invalid_address
-        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-      end
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
 
-    test 'email addresses should be unique' do
-      # creates user with same attributes as the one setup
-      duplicate_user = @user.dup
-      @user.save
-      assert_not duplicate_user.valid?
-    end
+  test 'email addresses should be unique' do
+    # creates user with same attributes as the one setup
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
-    test "email addresses should be saved as lower-case" do
-      mixed_case_email = "Foo@ExAMPle.CoM"
-      @user.email = mixed_case_email
-      @user.save
-      assert_equal mixed_case_email.downcase, @user.reload.email
-    end
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
-    test 'password should be present (nonblank)' do
-      @user.password = @user.password_confirmation = ' ' * 6
-      assert_not @user.valid?
-    end
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
 
-    test 'password should have a minimum length' do
-      @user.email = 'abc@abc.com'
-      @user.name = 'tester'
-      @user.password = @user.password_confirmation = 'a' * 5
-      assert_not @user.valid?
-    end
-    
-    test "authenticated? should return false for a user with nil digest" do
-      assert_not @user.authenticated?(:remember, '')
-    end
+  test 'password should have a minimum length' do
+    @user.email = 'abc@abc.com'
+    @user.name = 'tester'
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
+  end
 
-    test 'associated microposts should be destroyed' do
-      @user.save
-      @user.microposts.create!(content: 'Lorem ipsum')
-      assert_difference 'Micropost.count', -1 do
-        @user.destroy
-      end
+  test 'authenticated? should return false for a user with nil digest' do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
+  test 'associated microposts should be destroyed' do
+    @user.save
+    @user.microposts.create!(content: 'Lorem ipsum')
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
     end
+  end
+
+  test 'should follow and unfollow a user' do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+    # Users can't follow themselves.
+    michael.follow(michael)
+    assert_not michael.following?(michael)
+  end
 end
